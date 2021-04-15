89a998169d47f58105109df5392d671069c2375f
Raymond Eng <ray3ng@gmail.com>
2021-04-15 10:49:35 -0700

    Update work page

--- src/pages/work.jsx
+++ src/pages/work.jsx
@@ -7,6 +7,11 @@ import Badge from "../components/Badge";
 
 import jkcDashboardImg from "../images/jkcsales-dashboard.png";
 import jkcReportImg from "../images/jkcsales-report.png";
+import storyboardImg from "../images/storyboard-1.png";
+import personaImg from "../images/persona.png";
+import framerImg from "../images/framer.png";
+
+import intersectHomeVideo from "../images/intersect-home.mov";
 
 export const Work = () => (
   <>
@@ -14,7 +19,9 @@ export const Work = () => (
       <title>Raymond Eng - Work</title>
     </Helmet>
     <div className="container">
-      <h1 className="title--large">project 1.</h1>
+      <h1 className="title--large" id="jkcsales">
+        project 1.
+      </h1>
       <TextContainer title="JKCsales CRM Dashboard" subtitle="Web Application">
         <div>
           <Badge>AWS</Badge>
@@ -23,6 +30,8 @@ export const Work = () => (
           <Badge>Javascript</Badge>
         </div>
         <Image src={jkcDashboardImg} alt="JKC dashboard screenshot" />
+        <h6 className="subtitle--small">Role</h6>
+        <p>Full Stack - Independent Project</p>
         <h6 className="subtitle--small">Overview</h6>
         <p>
           Client required an application to keep track of his sale rep’s service
@@ -57,40 +66,73 @@ export const Work = () => (
         </p>
         <Image src={jkcReportImg} alt="JKCsales report user interface" />
       </TextContainer>
-      <h1 className="title--large">project 2.</h1>
-      <TextContainer title="Intersect" subtitle="Interactive prototype">
-        <div>
-          <Badge>Framer</Badge>
-          <Badge>Figma</Badge>
-          <Badge>UI/UX</Badge>
-        </div>
-        <h6 className="subtitle--small">Overview</h6>
-        <p>
-          My team was tasked with developing a high fidelity prototype for an
-          app idea. We decided on a dating mobile application based on proximity
-          based matching.
-        </p>
-        <h6 className="subtitle--small">Identify Problems</h6>
-        <p>
-          Developing relevant user features that help users accomplish their
-          goals.
-        </p>
-        <h6 className="subtitle--small">Solutions</h6>
-        <p>
-          Research User interviews were conducted to understand user goals
-          Ideation Personas were created to illustrate user needs Design
-          Wireframes and high fidelity prototypes were developed in Figma and
-          Framer. Test User testing with the high fidelity prototype was done to
-          identify problems Heuristic evaluations were performed to identify
-          issues.{" "}
-        </p>
-        <h6 className="subtitle--small">Reflection</h6>
-        <p>
-          This project taught me to align my design towards the user goals.
-          Without understanding the user or their problems, it is difficult to
-          solve their problems.
-        </p>
-      </TextContainer>
+      <div id="intersect">
+        <h1 className="title--large">project 2.</h1>
+        <TextContainer title="Intersect" subtitle="Interactive prototype">
+          <div>
+            <Badge>Framer</Badge>
+            <Badge>Figma</Badge>
+            <Badge>UI/UX</Badge>
+          </div>
+          <h6 className="subtitle--small">Role</h6>
+          <p>UX/UI Designer, Illustrator, Developer</p>
+          <h6 className="subtitle--small">Overview</h6>
+          <p>
+            My team was tasked with developing a high fidelity prototype for an
+            app idea. We decided on a dating mobile application based on
+            proximity based matching.
+          </p>
+          <h6 className="subtitle--small">Identify Problems</h6>
+          <p>
+            Developing relevant user features that help users accomplish their
+            goals.
+          </p>
+          <h6 className="subtitle--small">Solutions</h6>
+          <p>
+            Research User interviews were conducted to understand user goals
+            Ideation Personas were created to illustrate user needs Design
+            Wireframes and high fidelity prototypes were developed in Figma and
+            Framer. Test User testing with the high fidelity prototype was done
+            to identify problems Heuristic evaluations were performed to
+            identify issues.
+          </p>
+          <div className="flex-center">
+            <figure className="figure">
+              <img src={personaImg} alt="persona for intersect app" />
+              <figcaption>A peronsa developed for Intersect.</figcaption>
+            </figure>
+          </div>
+          <div className="flex-center">
+            <figure className="figure">
+              <img
+                src={storyboardImg}
+                alt="Intersect storyboard and user flows sketch"
+              />
+              <figcaption>Started with key interaction flows</figcaption>
+            </figure>
+          </div>
+          <div className="flex-center">
+            <figure className="figure">
+              <img
+                src={framerImg}
+                alt="Screenshot of many screens in Framer Prototying tool"
+              />
+              <figcaption>Framer prototyping screens</figcaption>
+            </figure>
+          </div>
+          <h6 className="subtitle--small">Reflection</h6>
+          <p>
+            This project taught me to align my design towards the user goals.
+            Without understanding the user or their problems, it is difficult to
+            solve their problems.
+          </p>
+          <div className="flex-center">
+            <video autoPlay muted loop class="video__intersect">
+              <source src={intersectHomeVideo} type="video/mp4" />
+            </video>
+          </div>
+        </TextContainer>
+      </div>
     </div>
     <Footer />
   </>
